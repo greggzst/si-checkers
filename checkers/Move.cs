336bcce97ddc73119d58b4b443eddb320f98feca
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-04-30 22:19:30 +0200

    Add Move class

---
+++ checkers/Move.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkers
+{
+    class Move
+    {
+        public int CurrentRow { get; set; }
+        public int CurrentCol { get; set; }
+        public int RowToBeMovedTo { get; set; }
+        public int ColToBeMovedTo { get; set; }
+
+        public Move(int cRow, int cCol, int mRow, int mCol)
+        {
+            CurrentRow = cRow;
+            CurrentCol = cCol;
+            RowToBeMovedTo = mRow;
+            ColToBeMovedTo = mCol;
+        }
+    }
+}
