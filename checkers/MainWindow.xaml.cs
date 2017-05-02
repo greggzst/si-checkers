f1288620d83260e01a27080a54a6cbb4f5a4c1f3
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-05-02 12:17:22 +0200

    Add insertPiecesOnBoard method

--- checkers/MainWindow.xaml.cs
+++ checkers/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace checkers
         {
             InitializeComponent();
             buildBoard();
-            
+
         }
 
         private void buildBoard()
@@ -49,19 +49,115 @@ namespace checkers
                     Grid.SetColumn(boardField, j);
                 }
             }
+
+            insertPiecesOnBoard();
         }
 
         private StackPanel getBoardField(Grid board, int row, int col)
         {
             for (int i = 0; i < board.Children.Count; i++)
             {
-                StackPanel field = (StackPanel) board.Children[i];
+                StackPanel field = (StackPanel)board.Children[i];
                 if (Grid.GetRow(field) == row && Grid.GetColumn(field) == col)
                     return field;
             }
             return null;
         }
 
-        
+        private void insertPiecesOnBoard()
+        {
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    StackPanel stackPanel = getBoardField(board, row, col);
+                    Button button = new Button();
+                    button.Click += new RoutedEventHandler(piece_click);
+                    button.Height = 50;
+                    button.Width = 50;
+                    button.HorizontalAlignment = HorizontalAlignment.Center;
+                    button.VerticalAlignment = VerticalAlignment.Center;
+                    var redBrush = new ImageBrush();
+                    redBrush.ImageSource = new BitmapImage(new Uri("images/red.png", UriKind.Relative));
+                    var blackBrush = new ImageBrush();
+                    blackBrush.ImageSource = new BitmapImage(new Uri("images/black.png", UriKind.Relative));
+
+                    switch (row)
+                    {
+                        case 0:
+                            if (col % 2 == 1)
+                            {
+
+                                button.Background = redBrush;
+                                button.Name = "buttonRed" + row + col;
+                                stackPanel.Children.Add(button);
+                            }
+                            break;
+                        case 1:
+                            if (col % 2 == 0)
+                            {
+                                button.Background = redBrush;
+                                button.Name = "buttonRed" + row + col;
+                                stackPanel.Children.Add(button);
+                            }
+                            break;
+                        case 2:
+                            if (col % 2 == 1)
+                            {
+                                button.Background = redBrush;
+                                button.Name = "buttonRed" + row + col;
+                                stackPanel.Children.Add(button);
+                            }
+                            break;
+                        case 3:
+                            if (col % 2 == 0)
+                            {
+                                button.Background = Brushes.Gray;
+                                button.Name = "button" + row + col;
+                                stackPanel.Children.Add(button);
+                            }
+                            break;
+                        case 4:
+                            if (col % 2 == 1)
+                            {
+                                button.Background = Brushes.Gray;
+                                button.Name = "button" + row + col;
+                                stackPanel.Children.Add(button);
+                            }
+                            break;
+                        case 5:
+                            if (col % 2 == 0)
+                            {
+                                button.Background = blackBrush;
+                                button.Name = "buttonBlack" + row + col;
+                                stackPanel.Children.Add(button);
+                            }
+                            break;
+                        case 6:
+                            if (col % 2 == 1)
+                            {
+                                button.Background = blackBrush;
+                                button.Name = "buttonBlack" + row + col;
+                                stackPanel.Children.Add(button);
+                            }
+                            break;
+                        case 7:
+                            if (col % 2 == 0)
+                            {
+                                button.Background = blackBrush;
+                                button.Name = "buttonBlack" + row + col;
+                                stackPanel.Children.Add(button);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        public void piece_click(Object sender, RoutedEventArgs e)
+        {
+        }
     }
 }
